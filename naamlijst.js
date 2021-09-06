af480eaa542e6201f870923f63ffbe85f1294496
Subaraki <robbeenaxel@hotmail.com>
2021-09-06 21:01:24 +0200

    naming list with counters

--- naamlijst.js
+++ naamlijst.js
@@ -1,12 +1,33 @@
 "use strict";
 
-const body = document.getElementsByTagName("body")[0];
-
 const unorganizedList = document.createElement("ul");
-const listItem = document.createElement("li");
+document.querySelector("body").appendChild(unorganizedList);
 
-for(const buttons of document.getElementsByTagName("button")){
-    buttons.onclick = function () {
+for (const button of document.getElementsByTagName("button")) {
+    button.onclick = function () {
+        const textFieldContent = document.getElementsByTagName("input")[0].value;
 
-    };
+        if (textFieldContent.length > 0) {
+
+            if (!checkHasNameAndUpdate(textFieldContent)) {
+                const listItem = document.createElement("li");
+                listItem.dataset.aantal = 1;
+                listItem.dataset.naam = textFieldContent;
+                listItem.innerText = `${listItem.dataset.naam}: ${listItem.dataset.aantal}`;
+                unorganizedList.appendChild(listItem)
+            }
+        }
+    }
+}
+
+function checkHasNameAndUpdate(nameToCheck) {
+    for (const entry of unorganizedList.children) {
+        if (entry.dataset.naam === nameToCheck) {
+            entry.dataset.aantal++;
+            found = true;
+            entry.innerText = `${entry.dataset.naam}: ${entry.dataset.aantal}`;
+            return true;
+        }
+    }
+    return false;
 }
