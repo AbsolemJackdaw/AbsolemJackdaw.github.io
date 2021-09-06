360729dff5ac33ef5c86e0e6b90635109b4da85a
Subaraki <robbeenaxel@hotmail.com>
2021-09-06 19:26:49 +0200

    index md to publish pages

---
+++ naamlijst.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const body = document.getElementsByTagName("body")[0];
+
+const unorganizedList = document.createElement("ul");
+const listItem = document.createElement("li");
+
+for(const buttons of document.getElementsByTagName("button")){
+    buttons.onclick = function () {
+
+    };
+}
