93097f21a809263320f3d99ce184475ef816d68a
Subaraki <robbeenaxel@hotmail.com>
2022-02-25 18:18:46 +0100

    fixed toggle memory

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict"
 
-function switchToLight() {
+const toggle = document.querySelectorAll("input");
+const theme = sessionStorage.getItem("theme")
+
+console.log(theme)
+if (theme === "light") {
+    switchToLight()
+    toggle[0].checked = true
+}
+
+
+function switchToDark() {
     const messages = document.querySelectorAll(".light");
     for (let index = 0; index < messages.length; index++) {
         messages[index].classList.remove("light");
@@ -8,7 +18,7 @@ function switchToLight() {
     }
 }
 
-function switchToDark() {
+function switchToLight() {
     const messages = document.querySelectorAll(".dark");
     for (let index = 0; index < messages.length; index++) {
         messages[index].classList.remove("dark");
@@ -16,12 +26,14 @@ function switchToDark() {
     }
 }
 
-const toggle = document.querySelectorAll("input");
-console.log(toggle[0]);
-toggle[0].onclick = function(){
+toggle[0].onclick = function () {
     if (toggle[0].checked) {
-        switchToDark();
-    } else {
         switchToLight();
+        sessionStorage.setItem("theme", "light")
+    } else {
+        switchToDark();
+        sessionStorage.setItem("theme", "dark")
     }
 }
+
+
