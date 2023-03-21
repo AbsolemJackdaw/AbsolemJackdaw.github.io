ff1133a095acb09a08ec855fd7160b9537b32df5
Subaraki <robbeenaxel@hotmail.com>
2023-03-21 22:09:08 +0100

    fix pwa

--- pwabuilder-adv-sw.js
+++ pwabuilder-adv-sw.js
@@ -1,3 +1,13 @@
 
-  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
-  precacheAndRoute([{"revision":"aace2f9cebac3bec0ea36f529fce22c2","url":"css/web_flex.css"},{"revision":"93e3790c3fbb32153f35b78762c46ba3","url":"css/web.css"},{"revision":"7f9a8bd8e0e48567df138e9907e97894","url":"index.html"},{"revision":"4d38e900913de0371fbfefbc259c7c11","url":"js/cardLoader.js"},{"revision":"e861454cde165236a9d2f2b6c617c6bd","url":"js/modcards.js"},{"revision":"72b5ddf2395743b85c303481fbee5d97","url":"js/script.js"},{"revision":"fe4a8ce3a5f941a9b59b9cef9009af28","url":"js/socialmediacards.js"},{"revision":"5f57f205f97cd3edbeec31b3456aa815","url":"mods.html"},{"revision":"dc1bd734b36e823a4fd62397646358fd","url":"service-worker.js"},{"revision":"436e1e440419030b633266cf7ce6a201","url":"story.html"}]);
+import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+precacheAndRoute([
+  { "revision": "aace2f9cebac3bec0ea36f529fce22c2", "url": "css/web_flex.css" },
+  { "revision": "93e3790c3fbb32153f35b78762c46ba3", "url": "css/web.css" },
+  { "revision": "92ba1bf4f59553e78f3c7035c5e9f5dd", "url": "index.html" },
+  { "revision": "4d38e900913de0371fbfefbc259c7c11", "url": "js/cardLoader.js" },
+  { "revision": "e861454cde165236a9d2f2b6c617c6bd", "url": "js/modcards.js" },
+  { "revision": "72b5ddf2395743b85c303481fbee5d97", "url": "js/script.js" },
+  { "revision": "fe4a8ce3a5f941a9b59b9cef9009af28", "url": "js/socialmediacards.js" },
+  { "revision": "cf585c6c0b3f39dd04e0312d9bdd606d", "url": "mods.html" },
+  { "revision": "ff5da0b292612186186be55009060875", "url": "service-worker.js" },
+  { "revision": "436e1e440419030b633266cf7ce6a201", "url": "story.html" }]);
