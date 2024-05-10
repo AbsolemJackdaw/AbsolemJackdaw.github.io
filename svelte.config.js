e4a69170bddf2006b57c91a0b2303d9e511c1c99
Axel Cornelis PXL <axel.Cornelis@student.pxl.be>
2024-05-10 16:39:16 +0200

    adjust static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -3,23 +3,26 @@ import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    adapter: adapter({
-        // default options are shown. On some platforms
-        // these options are set automatically — see below
-        pages: 'build',
-        assets: 'build',
-        fallback: undefined,
-        precompress: false,
-        strict: true
-    }),
     preprocess: vitePreprocess(),
+    trailingSlash: 'always',
     prerender: {
         default: true,
     },
-    trailingSlash: 'always',
-    paths: {
-        base: '/AbsolemJackdaw.github.io'
+    kit:{
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            base: '/Absolemackdaw.github.io'
+        }
     }
+
 };
 
 export default config;
