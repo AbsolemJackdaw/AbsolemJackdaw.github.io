2da67ad7274e84f7133075464ad6ff8d5e1328f2
Axel Cornelis PXL <axel.Cornelis@student.pxl.be>
2024-05-11 15:14:41 +0200

    remove title for consistency

--- src/routes/mods/+page.svelte
+++ src/routes/mods/+page.svelte
@@ -5,8 +5,8 @@
     const allCards = modCardRegistry();
 </script>
 
-<div class="flex flex-col items-center mt-5 text-4xl font-bold m-48">
-    <h1 class="mb-20">Welcome to the Subhaven</h1>
+<div class="flex flex-col items-center mt-5 text-4xl font-bold mx-48">
+    <!--    <h1 class="mb-20">Welcome to the Subhaven</h1>-->
     <div class="flex flex-wrap text-center justify-center">
         {#each allCards as card}
             <Card {card}/>
