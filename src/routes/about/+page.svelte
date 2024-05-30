4a3eb5ccdc8ae538f21d3c566bf293622c8b58f9
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-05-30 21:17:23 +0200

    add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,180 @@
+<script>
+    import Card from "../../component/Card.svelte";
+    import {mediaCardRegistry} from "../../component/cardRegistry";
+</script>
+
+<div class="my-10 py-10 px-5 flex flex-col xl:mx-[25%] mx-16 md:mx-32 backdrop-blur-sm rounded-2xl bg-[#000000aa] font-mono">
+    <div class="font-bold text-2xl text-center pb-5 border-b-2 border-pink-400 text-teal-400 mb-8">Welcome</div>
+    <p class="font-bold text-teal-300">My name is Absolem Jackdaw.</p>
+    <br/>
+    If you were to look up my actual name, you would find nothing of interest.
+    <p>Google Absolem Jackdaw, however, and you will find all of my achievements on multiple pages.</p>
+    <br/>
+    <ul class="list-disc list-inside space-y-2 mx-5">
+        <li>A youtube page with my own mod reviews to help the community understand what my mods are made of.</li>
+        <li>A twitter page dating back to 2013 when I was two years into starting my adventure into the world of
+            programming.
+        </li>
+        <li>My Curseforge Page, the staple of the collection of my work.</li>
+        <li>My DeviantArt profile, a page where I show of my artistic prowess</li>
+        <li>My GitHub pages, where you can find this very website</li>
+        <li>GitHub references to my repositories</li>
+        <li>A peek into my private life with the Honda Shadow Forums, where I share my findings repairing Honda
+            motorcycles.
+            (but I mostly ask questions)
+        </li>
+        <li>Cooperation legacy: LadySnake with whom I worked together for a while until their mod surpassed both my
+            free time
+            AND capability
+        </li>
+        <li>Cooperation two-point-oh: Darkhax is a person who I consider one of my best friends. He can ask me
+            anything
+            anytime and my answer will never be no. Our friendship has been molded into the internet. We're there. He
+            and me. In
+            a google search result. Together.
+        </li>
+        <li>The V4 forums! Throwback to me loving motorcycles 'till this day.</li>
+        <li>Legacy three-point-oh: One of my mods got ported by a fan. My name is on that website. I'm there. Work I
+            have made
+            got ported by a fan. It's on Google now.
+        </li>
+    </ul>
+    <br/>
+    There's even scam sites citing my work to try and entice unsuspecting people to install viruses on their PCs!
+    I mean, that's not good... but it means that I'm important enough to be used as bait? yay ?
+    <hr class="h-px my-8 border-1 border-cyan-400 mx-10">
+    <br/>
+    <p>Ego aside, here's a little bit about myself by myself :</p>
+    <br/>
+    <p>I have been trying to figure out who I am and what my purpose is for a while.</p>
+    <p>I've enjoyed a great upbringing but I didn't come out unscathed.</p>
+    <p>I have been diagnosed with ADHD at 31 and there are suspicions of autism. I have no fear of these.</p>
+    <p>Over time I have been able to learn to live with them to my own rhythm. The issues build up when the outside
+        is concerned.
+    </p>
+    <p>We're on our way to try and become our best selves.</p>
+    <br/>
+    <p> For those who wonder where the nickname comes from, here's the explanation:</p>
+    <p> Absolem is a derivative of the etymology of my birth-given name. It's the only handle I like so far.
+        I've went trough a handful of handles over the years, but none of them resounded with me. They were bound by
+        definitions of myself that people gave me.
+        Absolem is my own name, just a little bit encrypted.
+    </p>
+    <br/>
+    <p>The jackdaw part comes from my favourite bird. It's tattooed on my chest today.</p>
+    <p>One of my friends asked me : "A jackdaw? Really? They're scavengers!"</p>
+    <p>It's not about that though, it's what the bird represents.</p>
+    <br/>
+    <p>I'll never forget the day my parents dropped me and my brothers off at my grandparents. We stayed the night, and
+        the next morning me and my grandpa were looking out the window.
+    </p>
+    <br/>
+    <p>I loved the man. I still do. He had so much knowledge about nature, and I still love that today.</p>
+    <p> It was a winter morning. It was cold out. We could barely see across the street from the big window, But we
+        could see a couple poles and the barbed wire from across the street.
+    </p>
+    <br/>
+    Silence befell as we observed the cold day when a jackdaw landed on top op one of the poles that delimited the
+    field.<br/>
+    <p> He told me all about the jackdaw, about their white head and how we could recognize them by it from other black
+        birds.
+        Some memories are not made to die trough time. The old man awoke a flame in me; a love for nature and detail
+        that I carry with me to this day.
+    </p>
+    <br/>
+    <hr class="h-px my-8 border-1 border-cyan-400 mx-10">
+    <h3 class="font-bold">What are your plans for the future?</h3>
+    <p>I want to continue creating, preferably with Minecraft as a means, and gather people with the same vision to
+        start a
+        programming band. Preferably let it be a means for me to survive and/or live.
+    </p>
+    <br/>
+    <h3 class="font-bold">Will you ever drop modding?</h3>
+    <p>I have had multiple opportunities to stop. I will not. It keeps pulling me back. I love the creation and I love
+        the community.
+    </p>
+    <br/>
+    <h3 class="font-bold">Are you ever planning on becoming a well renowned programmer?</h3>
+    <p>I have no ambition of career. I just want to be able to pay rent and if Odin allows me to, one day buy a
+        house. But I just want a place to work where I'm comfortable and happy.
+    </p>
+    <br/>
+    <h3 class="font-bold">Did you study anything to become the awesome person you are today?</h3>
+    <p>Don't flatter me! No I didn't for a long time. Mainly self thought with a curiosity drive for the next
+        step. Starting 2021 I did take up studies to try and get a bachelor degree in software development as a full
+        stack dev at the PXL University at Hasselt.
+        As of currently I do not have a bachelors yet. If I had more drive for new technologies in the past, I could
+        have learned what they gave me by myself,
+        but I can't deny that in the three years I went there, I have learned a lot. It equally gave me the image of
+        self worth I was looking for for ages. I feel like my knowledge is legit now.
+    </p>
+    <br/>
+    <hr class="h-px my-8 border-1 border-cyan-400 mx-10">
+    <p class="font-bold text-xl">Why Minecraft?</p>
+    <p>Back when I was a teenager, Minecraft was released and I got to discover it around 2011 when one of my friends
+        was playing it on his laptop. I was intrigued.
+    </p>
+    <br/>
+    <p>Not too long after that I bought the game for myself and after playing a mere six hours, my fantasy was going
+        wild. I found some mods on the internet to install and before I knew it I was reading documentation on how to
+        make a mod myself.
+    </p>
+    <br/>
+    <p>The mods that I made back then are long lost to time. I didn't know what GitHub was yet and I was brand new to
+        programming altogether. There are some traces left in the dusty corners of youtube, in the category that has
+        seemingly died out since 2015. There are some mod reviews left of a tiny handful of mods that I found on
+        Youtube, dating back to 1.5.1, somewhere around 2011.
+    </p>
+    <br/>
+    <p>The creativity I could funnel into the game was unlimited. At this point in time, I had been looking for artistic
+        expression in the world of video games for a while already. Track Mania Nations had an editor and I can
+        remember tinkering with GameMaker before 2010. I have a vague memory of some kind of 3D-platformer in which I
+        tried to make some levels but none of them tickled my fancy. I can also remember some off-brand GameMaker
+        knockoffs; but they all had pre-made assets and every level I tried to make felt the same.
+    </p>
+    <br/>
+    <p>Minecraft however was a blank canvas. The art style is accessible, simple and square, and the worlds the game
+        creates are the very definition of a sandbox. There is nothing that defines a century or culture the game might
+        be set in. With the help some youtube tutorials made by <a class="underline underline-offset-2"
+                                                                   href="https://www.youtube.com/watch?v=L3FJnUZXUO8&list=PLB1D8D9B290C09EAB">x2Robbie2x</a>,
+        I was able to
+        let my creativity flow freely. My creativity is what has always primed in my mods, and will be the only drive
+        that justifies my mods. This mindset also defines the way I create my mods. I will always create mods out of
+        creative drive first, which in turn is fueled by a need of necessity, and never by demand.
+    </p><br/>
+    <p>The mod that made me realise this was Paintings++. The irony in the story is that it isn't my own creation.
+        Paintings++ was made after I found the original painting extension mod somewhere around 1.2 and saw that it was
+        no longer
+        maintained by the original owner, and already had had a couple of people trying to keep it alive at that point.
+        I found the mod when it was in an abandoned state and picked it up because I needed it.
+    </p>
+    <p>I needed more paintings in my game and there were no longer any options to do that. Turns out I wasn't the only
+        one with the opinion and lots of people started chipping in and saying how happy they were with my continuation
+        of the mod.</p>
+    <br/>
+    <p>The reason why I want to underline this mentality is because a lot of people start creating with the hopes of
+        their creation becoming the next best thing. Their work turns into labor over time and their hobby
+        becomes a burden. Even if their creation becomes a staple of the community, they quite often drop it soon after
+        because they cannot find the drive to continue maintaining the very thing that makes them wake up stressed. This
+        is the very thing I try to avoid for myself and to which I will never give in to.</p>
+    <br/>
+    <p>This doesn't mean that when I make a mod I refuse to cater to the people that use it. There's a right balance to
+        be found between I want and what the community wants. There's a fine line that we can thread to please the most
+        amount of people possible. The creation is the primary part, and sharing is a happy byproduct of the
+        process.</p>
+    <br>
+    <p>How I ever got from "Yay blocks" to "Damn, Tailwind isn't correctly formatting my Svelte page, yet it worked
+        correctly when I was using NextJS yesterday" will forever stay a mystery for me.</p>
+    <p>I call it 'my happy little accident'</p>
+    <br/>
+    <p></p>
+
+    <hr class="h-px my-8 border-1 border-cyan-400 mx-10">
+    <br/>
+    <p> If you have any questions, inqueries or want to know more about my work, me or my environment, feel
+        free to join me at my discord.
+    </p>
+    <div class="flex-col flex items-center justify-center">
+        <Card card={mediaCardRegistry()[2]}></Card>
+    </div>
+</div>
