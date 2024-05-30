1740f8b479c2b2615b3697c363f53cb31c937c9e
Absolem Jackdaw <absolemjackdaw@gmail.com>
2024-05-30 21:16:44 +0200

    spell checker. add a new section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,16 @@
     import Navbar from "../component/Navbar.svelte";
 </script>
 <style>
+    .mountainImg {
+        background-image: url("/mountain.svg");
+        background-repeat: repeat-x;
+        background-position: bottom;
+        background-attachment: fixed;
+    }
+
     .backgroundImg {
         background-image: url("/bg.svg");
-        background-repeat: repeat-x;
+        background-repeat: repeat;
         background-position: bottom;
         background-attachment: fixed;
     }
@@ -18,10 +25,12 @@
 </style>
 
 <body class="min-h-screen scroll backgroundImg text-slate-300">
+<div class="min-h-screen mountainImg">
 <div class="min-h-screen foregroundImg">
-    <Navbar/>
-    <div class="flex flex-col items-center pt-20">
-        <slot/>
+        <Navbar/>
+        <div class="flex flex-col items-center pt-20">
+            <slot/>
+        </div>
     </div>
 </div>
 </body>
